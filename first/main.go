4bab56b6a169e475b2364a600aad0c77f7c74fc6
Jan Prachař <jan.prachar@gmail.com>
2025-01-25 12:15:07 +0100

    refresh first

--- first/main.go
+++ first/main.go
@@ -7,14 +7,9 @@ import (
 	"log"
 	"os"
 	"sort"
-	"unicode/utf8"
 
 	"../dict"
-	"../progress"
-)
-
-const (
-	size = 5
+	pr "../progress"
 )
 
 type WeightedWord struct {
@@ -33,59 +28,14 @@ type LuckStat struct {
 	Sum       float64
 }
 
-type Tip struct {
-	Word        string
-	Left        int
-	LeftNotUsed int
-	Luck        *float64
-	Skill       *int
+type Skill struct {
+	Relative   int
+	Difficulty int
 }
 
-var letters = []rune{
-	'a',
-	'b',
-	'c',
-	'd',
-	'e',
-	'f',
-	'g',
-	'h',
-	'i',
-	'j',
-	'k',
-	'l',
-	'm',
-	'n',
-	'o',
-	'p',
-	'q',
-	'r',
-	's',
-	't',
-	'u',
-	'v',
-	'w',
-	'x',
-	'y',
-	'z',
-}
-
-func LoadLuck(filePath string) (map[string]*LuckStat, error) {
-	luck := make(map[string]*LuckStat)
-
-	f, err := os.Open(filePath)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	dec := gob.NewDecoder(f)
-	err = dec.Decode(&luck)
-	if err != nil {
-		return nil, err
-	}
-
-	return luck, nil
+type Skills struct {
+	Robot *Skill
+	Human *Skill // human nezná použitý slova
 }
 
 func CalculateOdds(
@@ -93,94 +43,85 @@ func CalculateOdds(
 	all []string,
 	history map[string]bool,
 	words dict.Dictionary,
-	ppr *progress.Progress,
+	ppr *pr.Progress,
 ) (
-	float64, *LuckStat,
+	float64, float64, *LuckStat,
 ) {
-	var sum float64
+	var sumNotUsed, sum float64
 	var luck LuckStat
-	var count int
+	var countNotUsed, count int
 
 	luck.Histogram = make(map[int]int)
 
 	for _, w := range all {
-		if history[w] {
-			continue
-		}
+		var counter, counterNotUsed int
 
-		var counter int
-
-		if w != word {
+		if pr.StripDiacritic(w) != word {
 			pr := ppr.Clone()
 			pr.ResetRound()
 
 			pr.Guess(word, w)
-			/*
-				if word == "hubka" {
-					var list []string
-					_, counter, list = pr.WordsLeft(words, true)
-					fmt.Printf("%s + %s: ", word, w)
-					fmt.Println(list)
-
-				} else {
-			*/
-			_, counter, _ = pr.WordsLeft(words, false)
+			counter, counterNotUsed, _ = pr.WordsLeft(words, false)
 		}
 
-		luck.Histogram[counter]++
-		luck.Sum++
 		sum += float64(counter)
 		count++
-	}
 
-	return sum / float64(count), &luck
-}
-
-func makeString(word []rune) string {
-	var buf []byte
-	for _, l := range word {
-		buf = utf8.AppendRune(buf, l)
-	}
-	return string(buf)
-}
-
-func AppendTip(tips []Tip, word string, counter, counterNotUsed int, luck map[string]*LuckStat, skill map[string]*int) []Tip {
-	tip := Tip{
-		Word:        word,
-		Left:        counter,
-		LeftNotUsed: counterNotUsed,
-	}
-
-	if wordLuck, found := luck[word]; found {
-		var sumBetter int
-		for histLeft, histCount := range wordLuck.Histogram {
-			if histLeft <= counterNotUsed {
-				sumBetter += histCount
-			}
+		if !history[w] {
+			luck.Histogram[counterNotUsed]++
+			luck.Sum++
+			sumNotUsed += float64(counterNotUsed)
+			countNotUsed++
 		}
-		luck := 100 - 100*float64(sumBetter)/(wordLuck.Sum)
-		tip.Luck = &luck
-		tip.Skill = skill[word]
 	}
 
-	return append(tips, tip)
+	return sum / float64(count), sumNotUsed / float64(countNotUsed), &luck
+}
+
+func CalculateSkill(words []WeightedWord) map[string]*Skill {
+	skill := make(map[string]*Skill)
+	tmpSkill := make([]struct {
+		w  string
+		sk int
+	}, len(words))
+
+	sk := 0
+	wg := words[0].Weight
+
+	for i, w := range words {
+		if w.Weight > wg {
+			sk++
+			wg = w.Weight
+		}
+		tmpSkill[i].w = w.Word
+		tmpSkill[i].sk = sk
+	}
+
+	maxSk := sk
+	for _, s := range tmpSkill {
+		sk := 0
+		if maxSk > 0 {
+			sk = (maxSk - s.sk) * 100 / maxSk
+		}
+		w := pr.StripDiacritic(s.w)
+		skill[w] = &Skill{Relative: sk, Difficulty: maxSk}
+	}
+
+	return skill
 }
 
 func LoadDictionary(filePath string) ([]string, error) {
 	words := make([]string, 2000)
-
 	f, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
 	}
 	defer f.Close()
-
 	s := bufio.NewScanner(f)
 
 	for s.Scan() {
 		words = append(words, s.Text())
 	}
-
 	return words, s.Err()
 }
 
@@ -191,7 +132,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	words, err := dict.LoadDictionary("db.txt", history)
+	words, err := dict.LoadDictionary("db-hacky.txt", history)
 	if err != nil {
 		fmt.Println("loading words failed", err)
 		os.Exit(1)
@@ -204,52 +145,33 @@ func main() {
 	}
 
 	luck := make(map[string]*LuckStat)
-	skill := make(map[string]*int)
-	progress := progress.NewProgress(size, letters)
+	skillHuman := make(map[string]*Skill)
+	skillRobot := make(map[string]*Skill)
+	progress := pr.NewProgress(5)
 
 	for i := 1; i <= 1; i++ {
-		wordsLeftWeighted := make([]WeightedWord, len(all))
-		for j, w := range all {
-			odds, wordLuck := CalculateOdds(w, all, history, words, progress)
-			wordsLeftWeighted[j] = WeightedWord{w, odds}
+		wordsLeftRobotWeighted := make([]WeightedWord, len(all))
+		wordsLeftHumanWeighted := make([]WeightedWord, len(all))
+
+		for j, slovo := range all {
+			w := pr.StripDiacritic(slovo)
+			oddsHuman, oddsRobot, wordLuck := CalculateOdds(w, all, history, words, progress)
+			wordsLeftRobotWeighted[j] = WeightedWord{slovo, oddsRobot}
+			wordsLeftHumanWeighted[j] = WeightedWord{slovo, oddsHuman}
 			luck[w] = wordLuck
-
 		}
-		sort.Sort(ByWeight(wordsLeftWeighted))
 
-		for _, w := range wordsLeftWeighted {
+		sort.Sort(ByWeight(wordsLeftHumanWeighted))
+		skillHuman = CalculateSkill(wordsLeftHumanWeighted)
+
+		sort.Sort(ByWeight(wordsLeftRobotWeighted))
+		skillRobot = CalculateSkill(wordsLeftRobotWeighted)
+
+		for _, w := range wordsLeftRobotWeighted {
 			if history[w.Word] {
 				w.Word += " *** "
 			}
 			fmt.Printf("%s %f\n", w.Word, w.Weight)
-		}
-		fmt.Println("")
-
-		tmpSkill := make([]struct {
-			w  string
-			sk int
-		}, len(wordsLeftWeighted))
-
-		sk := 0
-		wg := wordsLeftWeighted[0].Weight
-
-		for i, w := range wordsLeftWeighted {
-			if w.Weight > wg {
-				sk++
-				wg = w.Weight
-			}
-			tmpSkill[i].w = w.Word
-			tmpSkill[i].sk = sk
-		}
-
-		maxSk := sk
-		skill = make(map[string]*int)
-		for _, s := range tmpSkill {
-			sk := 0
-			if maxSk > 0 {
-				sk = (maxSk - s.sk) * 100 / maxSk
-			}
-			skill[s.w] = &sk
 		}
 	}
 
@@ -261,12 +183,16 @@ func main() {
 
 	enc := gob.NewEncoder(f)
 	err = enc.Encode(luck)
-
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = enc.Encode(skill)
 
+	err = enc.Encode(skillRobot)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = enc.Encode(skillHuman)
 	if err != nil {
 		log.Fatal(err)
 	}
